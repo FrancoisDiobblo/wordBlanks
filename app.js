4ecf3dff17d95a4548d5b8b5abbe800afbb563fd
Dioche Diobblo <diofrancois2013@gmail.com>
2020-12-02 05:41:37 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+
+var wordBlanks = "The" + " " + myNoun + " That is so" + " " + myAdjective + " " + 
+myVerb + " very " + myAdverb + " . "; 
